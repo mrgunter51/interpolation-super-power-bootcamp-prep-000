2ef42d486f2d47c29070aa73b2bdb7b89e7bee79
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-24 21:10:53 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -18,5 +18,5 @@ def display_rainbow(colors)
   end
 end
 
-colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+colors = ['red', 'orange', 'yellow', 'green']
 display_rainbow(colors)
